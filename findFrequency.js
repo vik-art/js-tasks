81a95a3312e541c8923603b37b9dcc3c5ebffb0e
Viktoriia <krjuchenkova@gmail.com>
2022-05-03 14:08:42 +0300

    second implementation added

--- findFrequency.js
+++ findFrequency.js
@@ -5,7 +5,17 @@ const findFrequency = arr => {
     
 }
 
-console.log(findFrequency([1, 1, 2, 3, 4, 5, 4, 5, 4, 5, 2]))
+const findFrequency2 = arr => {
+    const obj = {};
+    for(let i = 0; i < arr.length; i ++) {
+        const curr = arr[i];
+       obj[curr] ? obj[curr]++ : obj[curr] = 1;
+    }
+    return obj;
+}
+
+console.log(findFrequency([1, 1, 2, 3, 4, 5, 4, 5, 4, 5, 2]));
+console.log(findFrequency2([1, 1, 2, 3, 4, 5, 4, 5, 4, 5, 2]))
 
 // input: array;
 //  output: object, where:
